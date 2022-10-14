7565e574860689c2164e729d0495b86af62a8c06
nandaarrysaputra69 <tu.nandak.1@gmail.com>
2022-10-14 16:07:31 +0800

    First Commit

--- jquery.js
+++ jquery.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-    
+var selectedRow = null;
+var no = 1;
+
+$(document).ready(function formSubmit(){
+    var formData = readForm();
+    if (selectedRow == null)
+        insertData (formData);
+    else
+        updateData (formData);
+    resetForm();
+});
+
+$(document).ready(function readForm(){
+    var formData = {};
+    formData["nim"] = $('#nim').value;
+    formData["name"] = $('#name').value;
+    return formData;
+});
+
+$(document).ready(function insertData(data){
+   var table = $('#tabledata').$('tbody')[0];
+   var newRow = $
 });
