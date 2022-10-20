4c7c7f7b509870e90fea395086039d0f609eede3
nandaarrysaputra69 <tu.nandak.1@gmail.com>
2022-10-20 20:20:16 +0800

    Fifth Commit

--- jquery.js
+++ jquery.js
@@ -1,23 +1,61 @@
-var selectedRow = null;
-var no = 1;
+i = 1;
 
-$(document).ready(function formSubmit(){
-    var formData = readForm();
-    if (selectedRow == null)
-        insertData (formData);
-    else
-        updateData (formData);
-    resetForm();
+$("form").submit(function(e){
+    e.preventDefault();
+
+    var no = i++;
+    var nim = $("input[name='nim']").val();
+    var name = $("input[name='name']").val();
+
+    $("#tabledata tbody").append(
+        "<tr data-no='" + no
+        + "'data-nim='" + nim
+        + "'data-name='" + name
+        + "'><td>" + no
+        + "</td><td>" + nim
+        + "</td><td>" + name
+        + "</td><td><a class='btn-edit' type='button'>Edit | </a><a class='btn-delete' type='button'>Delete</a></td></tr>"
+    );
+
+    $("input[name='']").val("");
 });
 
-$(document).ready(function readForm(){
-    var formData = {};
-    formData["nim"] = $('#nim').value;
-    formData["name"] = $('#name').value;
-    return formData;
+$("body").on("click", ".btn-edit", function(){
+    var nim = $(this).parents("tr").attr("data-nim");
+    var name = $(this).parents("tr").attr("data-name");
+  
+    $(this)
+      .parents("tr")
+      .find("td:eq(1)")
+      .html("<input nim = 'edit-nim'value='" + nim + "'>");
+
+    $(this)
+      .parents("tr")
+      .find("td:eq(2)")
+      .html("<input name = 'edit-name'value='" + name + "'>");
+  
+    $(this)
+      .parents("tr")
+      .find("td:eq(3)")
+      .prepend("<a class='btn-update' type='button'>Update | </a>");
+  
+    $(this).hide();
 });
 
-$(document).ready(function insertData(data){
-   var table = $('#tabledata').$('tbody')[0];
-   var newRow = $
+$("body").on("click", ".btn-delete", function(){
+  $(this).parents("tr").remove();
+});
+
+$("body").on("click", ".btn-update", function(){
+  var nim = $(this).parents("tr").find("input[name='edit-nim']").val();
+  var name = $(this).parents("tr").find("input[name='edit-name']").val();
+
+  $(this).parents("tr").find("td:eq(1)").text(nim);
+  $(this).parents("tr").find("td:eq(2)").text(name);
+
+  $(this).parents("tr").attr("data-nim", nim);
+  $(this).parents("tr").attr("data-name", name);
+
+  $(this).parents("tr").find(".btn-edit").show();
+  $(this).parents("tr").find(".btn-update").remove();
 });
